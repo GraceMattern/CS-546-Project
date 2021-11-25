69db6bf2d29f11231a0175700f1d24789f9c74fc
maorm200 <maorm200@gmail.com>
2021-11-25 10:34:10 -0500

    adding components and scss

---
+++ 546-front-end/src/components/BankBanner.js
@@ -0,0 +1,12 @@
+const UserWelcome = () => {
+    return (
+        <div className="buttons-menu">
+            <h1>Bank-546</h1>
+            <h3>the only bank for your needs</h3>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </div>
+    )
+}
+
+export default UserWelcome;
---
+++ 546-front-end/src/components/UserWelcome.js
@@ -0,0 +1,14 @@
+const UserWelcome = () => {
+  return (
+    <div className="sidebar">
+      <div className="buttons-menu">
+        <h1>Bank-546</h1>
+        <h3>the only bank for your needs</h3>
+        <button>Sign Up</button>
+        <button>Log In</button>
+      </div>
+    </div>
+  );
+};
+
+export default UserWelcome;
