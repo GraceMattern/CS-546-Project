27081a25a934db7ac618928bca9df5d2586196c7
maorm200 <maorm200@gmail.com>
2021-11-28 22:04:40 -0500

    home page design done, need to add form

--- 546-front-end/src/components/BankBanner.js
+++ 546-front-end/src/components/BankBanner.js
@@ -1,10 +1,20 @@
+import DonutLarge from "../images/donut_large.svg";
+import EuroSymbol from "../images/euro_symbol.svg";
+import TrendingUp from "../images/trending_up.svg";
 const BankBanner = () => {
-    return (
-        <div className="bank-banner">
-            <h1>The best bank for your needs</h1>
-            <h3>Stocks, investments, we have it all!</h3>
-        </div>
-    )
-}
+  return (
+    <div className="bank-banner">
+      <div className="text">
+        <h1>The best bank for your needs</h1>
+        <h3>Stocks, investments, we have it all!</h3>
+      </div>
+      <div className="images">
+        <img src={EuroSymbol} alt="EuroSymbol" />
+        <img src={DonutLarge} alt="DonutLarge" />
+        <img src={TrendingUp} alt="EuroSymbol" />
+      </div>
+    </div>
+  );
+};
 
 export default BankBanner;
--- 546-front-end/src/components/UserWelcome.js
+++ 546-front-end/src/components/UserWelcome.js
@@ -1,11 +1,29 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
 const UserWelcome = () => {
+  const [userWelcomeState, setUserWelcomeState] = useState("home");
   return (
     <div className="sidebar">
       <div className="buttons-menu">
         <h1>Bank-546</h1>
-        <h3>the only bank for your needs</h3>
-        <button>Sign Up</button>
-        <button>Log In</button>
+        <h3>
+          the <strong>only</strong> bank for your needs
+        </h3>
+        {userWelcomeState === "home" && (
+          <div className="buttons-div">
+            <button onClick={() => setUserWelcomeState("signup")}>
+              Sign Up
+            </button>
+            <button onClick={() => setUserWelcomeState("login")}>Log In</button>
+          </div>
+        )}
+        {userWelcomeState === "login" && (
+          <form className="form-div" noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Username" />
+            <TextField id="standard-basic" label="Password" />
+            <button onClick={() => setUserWelcomeState("")}>Log In</button>
+          </form>
+        )}
       </div>
     </div>
   );
