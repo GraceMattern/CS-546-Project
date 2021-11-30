59aeb88746a09da30573fd3c18feb6fdace9660d
ChengChen <cchen80@stevens.edu>
2021-11-29 20:04:17 -0500

    update seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -14,27 +14,27 @@ async function main() {
     const johnId = john._id;
 
     const adam = await users.createUser('Adam', 'Smith', 'Citi', 'asmith@citi.com', 'test', 25);
-    const adamId = adam._id.toString();
+    const adamId = adam._id;
 
     const alice = await users.createUser('Alice', 'Cooper', 'Chase', 'acooper@chase.com', 'root', 20);
-    const aliceId = alice._id.toString();
+    const aliceId = alice._id;
 
     const sally = await users.createUser('Sally', 'Winters', 'Bank of America', 'swinters@bankofamerica.com', 'acb123', 26);
-    const sallyId = sally._id.toString();
+    const sallyId = sally._id;
 
 
     //Create account
 
-    const johnAccount = await accounts.createAccount(johnId,"saving");
+    const johnAccount = await accounts.createAccount(johnId.toString(),"saving");
     const johnAccountId = johnAccount._id.toString();
 
-    const adamAccount = await accounts.createAccount(adamId,"saving");
+    const adamAccount = await accounts.createAccount(adamId.toString(),"saving");
     const adamAccountId = adamAccount._id.toString();
 
-    const aliceAccount = await accounts.createAccount(aliceId,"saving");
+    const aliceAccount = await accounts.createAccount(aliceId.toString(),"saving");
     const aliceAccountId = aliceAccount._id.toString();
 
-    const sallyAccount = await accounts.createAccount(sallyId,"saving");
+    const sallyAccount = await accounts.createAccount(sallyId.toString(),"saving");
     const sallyAccountId = sallyAccount._id.toString();
 
 	console.log('Done seeding database');
