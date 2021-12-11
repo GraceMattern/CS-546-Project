0ead5de591a67068954fe1d0af23ac6fe318f74e
GraceMattern <56277939+GraceMattern@users.noreply.github.com>
2021-12-11 16:43:53 -0500

    monogConnection
    was missing from config folder on main branch

---
+++ config/mongoConnection.js
@@ -0,0 +1,25 @@
+const MongoClient = require("mongodb").MongoClient;
+const settings = {
+  mongoConfig: {
+    serverUrl: "mongodb://localhost:27017/",
+    database: "CS546B-Group6-Project",
+  },
+};
+const mongoConfig = settings.mongoConfig;
+
+let _connection = undefined;
+let _db = undefined;
+
+module.exports = {
+  connectToDb: async () => {
+    if (!_connection) {
+      _connection = await MongoClient.connect(mongoConfig.serverUrl);
+      _db = await _connection.db(mongoConfig.database);
+    }
+
+    return _db;
+  },
+  closeConnection: () => {
+    _connection.close();
+  },
+};
